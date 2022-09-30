0906f3a25cca95ca1b1409f548b4ddfee38e6c69
Bayzid Hossain Shahed <bayzid18103384@gmail.com>
2022-10-01 01:24:05 +0600

    updated

--- app.js
+++ app.js
@@ -13,15 +13,55 @@
 // const wrap = document.querySelector('wrapper');
 // console.log(wrap);
 
-const wmf = document.querySelector('#book-list li:nth-child(2) .name');
+// const wmf = document.querySelector('#book-list li:nth-child(2) .name');
 // console.log(wmf);
 
-var books = document.querySelector('#book-list li .name');
+// var books = document.querySelector('#book-list li .name');
 // console.log(books);
 
-books = document.querySelectorAll('#book-list li .name');
+// books = document.querySelectorAll('#book-list li .name');
 // console.log(books);
 
-Array.from(books).forEach(function(book){
-    console.log(book);
-});
+// Array.from(books).forEach(function(book){
+//     console.log(book);
+// });
+
+// const banner = document.querySelector('#page-banner');
+// console.log('#page-banner node type is:'.banner.nodeType);
+// console.log('#page-banner node name is:'.banner.nodeName);
+// console.log('#page-banner has child nodes:'.banner.hasChildNodes());
+
+
+// const clonedBanner = banner.cloneNode(true);
+// console.log(clonedBanner);
+
+// const bookList = document.querySelector('#book-list')
+// console.log('The parent node is:'.bookList.parentNode);
+// console.log('The parent element is:'.bookList.parentElement);
+
+// console.log(bookList.childNodes);
+// console.log(bookList.children);
+
+// console.log('book-list next siblings is:'.bookList.nextSibling);
+// console.log('book-list next siblings is:'.bookList.nextElementSibling);
+
+// console.log('book-list next siblings is:'.bookList.previousSibling);
+// console.log('book-list next siblings is:'.bookList.previousElementSibling);
+
+// bookList.previousElementSibling.querySelector('p').innerHTML += '<br/> Too cool for everyone else!';
+
+// var btns = document.querySelectorAll('#book-list .delete');
+
+// Array.from(btns).forEach(function(btn){
+//     btn.addEventListener('click', function(e) {
+//         const li = e.target.parentElement;
+//         li.parentNode.removeChild(li)
+//     });
+// });
+
+// const link = document.querySelector('#page-banner a');
+
+// link.addEventListener('click', function(e){
+//     e.preventDefault();
+//     console.log('navigation to', e.target.textContent,'was prevented');
+// })
